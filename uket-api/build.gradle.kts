33ef53ae43843588171ce72af2a372ad4ea9b9c4
0703kyj <kyj0703@konkuk.ac.kr>
2024-12-10 20:53:55 +0900

    chore: 패키지 구조 구성

--- uket-api/build.gradle.kts
+++ uket-api/build.gradle.kts
@@ -1,8 +1,3 @@
-dependencies {
-    implementation(project(":uket-api:infra"))
-    implementation(project(":uket-api:gateway"))
-}
-
 allprojects {
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter-web")
--- uket-api/gateway/build.gradle.kts
+++ uket-api/gateway/build.gradle.kts
@@ -1,10 +1,15 @@
 dependencies {
+    implementation(project(":uket-api:infra"))
+
     implementation("org.springframework.boot:spring-boot-starter-security")
 
     // jwt
     implementation("io.jsonwebtoken:jjwt-api:0.12.3")
     implementation("io.jsonwebtoken:jjwt-impl:0.12.3")
     implementation("io.jsonwebtoken:jjwt-jackson:0.12.3")
+
+    // swagger
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
 
     testImplementation("org.springframework.security:spring-security-test")
 }
