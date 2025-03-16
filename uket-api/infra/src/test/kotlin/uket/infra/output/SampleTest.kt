0341eff971de0def1828930a257b7e3b3e3d6522
nonaninona <starcraft0529@gmail.com>
2025-03-16 17:40:15 +0900

    test : 테스트 환경 구성

---
+++ uket-api/infra/src/test/kotlin/uket/infra/output/SampleTest.kt
@@ -0,0 +1,28 @@
+package uket.infra.output
+
+import jakarta.persistence.EntityManager
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+import uket.infra.output.persistence.entity.user.UserEntity
+
+@DataJpaTest
+class SampleTest {
+
+    @Autowired
+    lateinit var em : EntityManager
+
+    @Test
+    @Transactional
+    fun test() {
+        val user = UserEntity(name = "nameA")
+        em.persist(user)
+
+        val findUser = em.find(UserEntity::class.java, 1L)
+        Assertions.assertThat(findUser.name).isEqualTo(user.name)
+    }
+
+}
