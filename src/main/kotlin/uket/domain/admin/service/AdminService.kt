69cc4ffc2a546b48f7b38aec3188e3d1f9a6e22a
nonaninona <starcraft0529@gmail.com>
2025-03-26 14:44:59 +0900

    feat : AdminService 도메인 custom exception -> 기본 예외로 변경

--- src/main/kotlin/uket/domain/admin/service/AdminService.kt
+++ src/main/kotlin/uket/domain/admin/service/AdminService.kt
@@ -1,12 +1,11 @@
 package uket.uket.domain.organization.service
 
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import uket.uket.common.ErrorCode
 import uket.uket.domain.organization.dto.CreateAdminCommand
 import uket.uket.domain.organization.dto.CreateAdminWithoutPasswordCommand
 import uket.uket.domain.organization.entity.Admin
-import uket.uket.domain.organization.exception.OrganizationException
 import uket.uket.domain.organization.repository.AdminRepository
 
 @Service
@@ -14,18 +13,23 @@ import uket.uket.domain.organization.repository.AdminRepository
 class AdminService(
     val adminRepository: AdminRepository,
 ) {
+    fun findById(adminId: Long): Admin {
+        val admin = adminRepository.findByIdOrNull(adminId)
+            ?: throw IllegalStateException("해당 어드민을 찾을 수 없습니다")
+        return admin
+    }
+
     fun findByEmail(email: String): Admin = adminRepository.findByEmail(email)
-        ?: throw OrganizationException(ErrorCode.NOT_FOUND_EMAIL_OF_ADMIN)
+        ?: throw IllegalStateException("어드민의 이메일을 찾을 수 없습니다.")
 
     @Transactional
     fun createAdmin(createAdminCommand: CreateAdminCommand) {
         if (java.lang.Boolean.TRUE == adminRepository.existsByEmail(createAdminCommand.email)) {
-            throw OrganizationException(ErrorCode.ALREADY_EXIST_ADMIN)
+            throw IllegalStateException("이미 가입된 어드민입니다.")
         }
 
         val admin = Admin(
-            _id = 0L,
-            _organization = createAdminCommand.organization,
+            organization = createAdminCommand.organization,
             name = createAdminCommand.name,
             email = createAdminCommand.email,
             isSuperAdmin = false,
@@ -43,12 +47,11 @@ class AdminService(
     @Transactional
     fun createAdminWithoutPassword(createAdminWithoutPasswordCommand: CreateAdminWithoutPasswordCommand) {
         if (java.lang.Boolean.TRUE == adminRepository.existsByEmail(createAdminWithoutPasswordCommand.email)) {
-            throw OrganizationException(ErrorCode.ALREADY_EXIST_ADMIN)
+            throw IllegalStateException("이미 가입된 어드민입니다.")
         }
 
         val admin = Admin(
-            _id = 0L,
-            _organization = createAdminWithoutPasswordCommand.organization,
+            organization = createAdminWithoutPasswordCommand.organization,
             name = createAdminWithoutPasswordCommand.name,
             email = createAdminWithoutPasswordCommand.email,
             isSuperAdmin = false,
--- src/main/kotlin/uket/domain/admin/service/OrganizationService.kt
+++ src/main/kotlin/uket/domain/admin/service/OrganizationService.kt
@@ -2,15 +2,16 @@ package uket.uket.domain.organization.service
 
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import uket.uket.common.ErrorCode
 import uket.uket.domain.organization.entity.Organization
-import uket.uket.domain.organization.exception.OrganizationException
 import uket.uket.domain.organization.repository.OrganizationRepository
 
 @Service
 class OrganizationService(
     val organizationRepository: OrganizationRepository,
 ) {
-    fun findById(organizationId: Long): Organization = organizationRepository.findByIdOrNull(organizationId)
-        ?: throw OrganizationException(ErrorCode.NOT_FOUND_ORGANIZATION)
+    fun findById(organizationId: Long): Organization {
+        val organization = organizationRepository.findByIdOrNull(organizationId)
+            ?: throw IllegalStateException("단체를 찾을 수 없습니다.")
+        return organization
+    }
 }
