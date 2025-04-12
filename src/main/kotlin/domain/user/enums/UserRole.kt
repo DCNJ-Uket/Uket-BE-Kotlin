0d163b5a55f2e6c6d8b2c1132014d8343ca051d7
sam971114 <simonana@naver.com>
2025-04-12 13:32:12 +0900

    feat: ktlint checkt

--- src/main/kotlin/domain/user/enums/UserRole.kt
+++ src/main/kotlin/domain/user/enums/UserRole.kt
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:src/main/kotlin/uket/domain/user/enums/UserRole.kt
-package uket.uket.domain.user.enums
-=======
 package uket.domain.user.enums
->>>>>>> fb3d020b7c501bd1b91d421172155b30cb8d47f3:src/main/kotlin/domain/user/enums/UserRole.kt
 
 enum class UserRole {
     USERS,
