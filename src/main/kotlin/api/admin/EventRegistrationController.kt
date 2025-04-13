1c0bf9a09f7c3e3e3b5e2c5d4b837fea42346bcb
0703kyj <kyj0703@konkuk.ac.kr>
2025-04-13 17:07:42 +0900

    [UKET-65] 행사 등록 단건 조회 API 추가

--- src/main/kotlin/api/admin/EventRegistrationController.kt
+++ src/main/kotlin/api/admin/EventRegistrationController.kt
@@ -1,5 +1,6 @@
 package uket.api.admin
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import uket.api.admin.request.RegisterUketEventRequest
 import uket.api.admin.response.RegisterUketEventResponse
+import uket.api.admin.response.UketRegistrationEventResponse
 import uket.common.enums.EventType
 import uket.domain.admin.service.OrganizationService
 import uket.domain.eventregistration.service.EventRegistrationService
@@ -16,7 +18,20 @@ class EventRegistrationController(
     private val organizationService: OrganizationService,
     private val eventRegistrationService: EventRegistrationService,
 ) {
-    @PostMapping("/admin/organizations/{organizationId}/uket-event-registrations/event-type/{eventType}")
+    @GetMapping("/admin/uket-event-registrations")
+    fun getUketEventRegistrations(): List<UketRegistrationEventResponse> {
+        return listOf()
+    }
+
+    @GetMapping("/admin/uket-event-registrations/{uketEventRegistrationId}")
+    fun getUketEventRegistration(
+        @PathVariable("uketEventRegistrationId") uketEventRegistrationId: Long,
+    ): UketRegistrationEventResponse {
+        val eventRegistration = eventRegistrationService.getByIdWithEventRoundAndEntryGroup(uketEventRegistrationId)
+        return UketRegistrationEventResponse.from(eventRegistration)
+    }
+
+    @PostMapping("/admin/uket-event-registrations/organizations/{organizationId}/event-type/{eventType}")
     fun registerUketEvent(
         @RequestParam("organizationId") organizationId: Long,
         @PathVariable("eventType") eventType: EventType,
