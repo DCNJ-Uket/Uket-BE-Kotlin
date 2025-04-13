e51ee2f8d15e91ce82c36a2d52b3e9d1e73dcf23
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-04-13 17:58:06 +0900

    [UKET-80] 어드민 전체 목록 페이징 조회 시, organizationName도 함께 전달 (#15)
    * feat : 어드민 전체 목록 조회 시, organizationName도 함께 전달
    
    * file : 이름 변경 AdminWithOrganizationIdAndName -> AdminWithOrganizationDto
    
    * fix : merge 이후 오류 수정

--- src/main/kotlin/domain/admin/dto/AdminWithOrganizationDto.kt
+++ src/main/kotlin/domain/admin/dto/AdminWithOrganizationDto.kt
@@ -2,18 +2,20 @@ package uket.domain.admin.dto
 
 import uket.domain.admin.entity.Admin
 
-data class AdminWithOrganizationIdDto(
+data class AdminWithOrganizationDto(
     val id: Long,
     val organizationId: Long,
+    val organizationName: String,
     val name: String,
     val email: String,
     var password: String?,
     val isSuperAdmin: Boolean,
 ) {
     companion object {
-        fun from(admin: Admin): AdminWithOrganizationIdDto = AdminWithOrganizationIdDto(
+        fun from(admin: Admin): AdminWithOrganizationDto = AdminWithOrganizationDto(
             id = admin.id,
             organizationId = admin.organization.id,
+            organizationName = admin.organization.name,
             name = admin.name,
             email = admin.email,
             password = admin.password,
