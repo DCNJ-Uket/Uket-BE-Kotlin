10774d02feac3ebf37e1ffb44bb8d9c751d994a5
nonaninona <starcraft0529@gmail.com>
2025-05-03 21:23:33 +0900

    feat : 현재 시간 이후의 입장 그룹만 조회하도록 수정

--- src/main/kotlin/uket/domain/uketevent/repository/EntryGroupRepository.kt
+++ src/main/kotlin/uket/domain/uketevent/repository/EntryGroupRepository.kt
@@ -1,8 +1,16 @@
 package uket.domain.uketevent.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import uket.domain.uketevent.entity.EntryGroup
+import java.time.LocalDateTime
 
 interface EntryGroupRepository : JpaRepository<EntryGroup, Long> {
-    fun <T> findByUketEventRoundId(uketEventRoundId: Long, type: Class<T>): List<T>
+    @Query(
+        """
+            SELECT eg FROM EntryGroup eg
+            WHERE eg.entryStartDateTime >= :at
+        """
+    )
+    fun findByUketEventRoundIdAfter(uketEventRoundId: Long, at: LocalDateTime): List<EntryGroup>
 }
