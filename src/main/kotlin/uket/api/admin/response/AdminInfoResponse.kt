1ee261efee2362e9d9f0e6b05c85af2ab83b32c3
sam971114 <56977023+sam971114@users.noreply.github.com>
2025-05-05 18:12:16 +0900

    [UKET-140] 어드민 상세 조회 응답 필드 추가 (#46)
    * feat: 초대 메시지 변경
    
    * feat: password validation 추가
    
    * feat: login시에 public message 추가
    
    * feat: 회원가입 시 validation public message 추가
    
    * feat: adminInfoResponse organizationId 추가
    
    * feat: redis key,value string으로 통일
    
    * feat: ktlintCheck fix
    
    * feat: email Token 유효기간 변경

--- src/main/kotlin/uket/api/admin/response/AdminInfoResponse.kt
+++ src/main/kotlin/uket/api/admin/response/AdminInfoResponse.kt
@@ -5,6 +5,7 @@ import uket.domain.admin.dto.AdminWithOrganizationDto
 data class AdminInfoResponse(
     val adminId: Long,
     val organization: String,
+    val organizationId: Long,
     val name: String,
     val email: String,
     val isSuperAdmin: Boolean,
@@ -13,6 +14,7 @@ data class AdminInfoResponse(
         fun from(adminInfo: AdminWithOrganizationDto): AdminInfoResponse = AdminInfoResponse(
             adminId = adminInfo.id,
             organization = adminInfo.organizationName,
+            organizationId = adminInfo.organizationId,
             name = adminInfo.name,
             email = adminInfo.email,
             isSuperAdmin = adminInfo.isSuperAdmin,
