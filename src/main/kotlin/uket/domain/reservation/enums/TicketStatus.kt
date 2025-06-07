a60fb94c7704182049e79f69983237280c1a46e5
nonaninona <starcraft0529@gmail.com>
2025-06-07 18:49:31 +0900

    style : klint 에러 수정

--- src/main/kotlin/uket/domain/reservation/enums/TicketStatus.kt
+++ src/main/kotlin/uket/domain/reservation/enums/TicketStatus.kt
@@ -9,7 +9,8 @@ enum class TicketStatus(
     BEFORE_PAYMENT("입금 확인중", "예약은 완료되었으나 아직 해당 티켓 금액이 입금되지 않았습니다."),
     RESERVATION_CANCEL("예매 취소", "예매가 취소되었습니다. 해당 티켓으로 입장하실 수 없습니다."),
     EXPIRED("기간 만료", "이미 기간이 지난 티켓입니다. 날짜를 확인해주세요."),
-    REFUND_REQUESTED("환불 요청", "환불이 요청된 티켓입니다. 해당 티켓으로 입장하실 수 없습니다.");
+    REFUND_REQUESTED("환불 요청", "환불이 요청된 티켓입니다. 해당 티켓으로 입장하실 수 없습니다."),
+    ;
 
     companion object {
         val notActiveStatuses: List<TicketStatus> = mutableListOf(RESERVATION_CANCEL)
