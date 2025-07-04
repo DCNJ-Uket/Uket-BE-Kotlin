a9a907156712cefc8a8cfac55092a7b1d351d633
nonaninona <53222283+nonaninona@users.noreply.github.com>
2025-07-04 09:56:40 +0900

    [UKET-229] 티켓 예매 시, 공연자(지인)가 없는 경우 처리 (#101)
    * fix : 공연자(지인)가 없는 경우(= "") 지인 인원 추가 x
    
    * feat : admin이 ticket 취소 시, 공연자 별 ticket 갯수 감소 처리
    
    * feat : admin이 ticket 취소 시, 공연자 별 ticket 갯수 감소 처리

--- src/main/kotlin/uket/facade/UpdateTicketStatusFacade.kt
+++ src/main/kotlin/uket/facade/UpdateTicketStatusFacade.kt
@@ -5,6 +5,7 @@ import uket.domain.reservation.entity.Ticket
 import uket.domain.reservation.enums.TicketStatus
 import uket.domain.reservation.service.TicketService
 import uket.domain.uketevent.service.EntryGroupService
+import uket.domain.uketevent.service.PerformerService
 import uket.domain.uketevent.service.UketEventService
 import uket.domain.user.service.UserService
 import uket.modules.redis.aop.DistributedLock
@@ -16,11 +17,14 @@ class UpdateTicketStatusFacade(
     private val ticketingCompletionMessageSendService: TicketingCompletionMessageSendService,
     private val userService: UserService,
     private val uketEventService: UketEventService,
+    private val performerService: PerformerService,
 ) {
     @DistributedLock(key = "'ticketing' + #entryGroupId")
     fun updateTicketStatus(entryGroupId: Long, ticketId: Long, ticketStatus: TicketStatus, userId: Long): Ticket {
         if (ticketStatus === TicketStatus.RESERVATION_CANCEL) {
             entryGroupService.decreaseReservedCount(entryGroupId)
+
+            reduceTicketCountForPerformer(ticketId, entryGroupId)
         }
         if (ticketStatus == TicketStatus.BEFORE_ENTER) {
             val user = userService.getById(userId)
@@ -39,5 +43,14 @@ class UpdateTicketStatusFacade(
             )
         }
         return ticketService.updateTicketStatus(ticketId, ticketStatus)
+    }
+
+    private fun reduceTicketCountForPerformer(ticketId: Long, entryGroupId: Long) {
+        val ticket = ticketService.getById(ticketId)
+        if (ticket.performerName.isNotEmpty()) {
+            val entryGroup = entryGroupService.getById(entryGroupId)
+            val performer = performerService.findByNameAndRoundId(ticket.performerName, entryGroup.uketEventRoundId)
+            performerService.minusTicketCountForPerformer(performer.id, 1);
+        }
     }
 }
