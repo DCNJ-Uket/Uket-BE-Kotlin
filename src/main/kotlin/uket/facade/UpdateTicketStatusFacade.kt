dc7da66d3f4b9c17eb603e45d32cab2146bcbb1a
nonaninona <starcraft0529@gmail.com>
2025-07-04 09:49:11 +0900

    feat : admin이 ticket 취소 시, 공연자 별 ticket 갯수 감소 처리

--- src/main/kotlin/uket/facade/UpdateTicketStatusFacade.kt
+++ src/main/kotlin/uket/facade/UpdateTicketStatusFacade.kt
@@ -5,6 +5,7 @@ import uket.domain.reservation.entity.Ticket
 import uket.domain.reservation.enums.TicketStatus
 import uket.domain.reservation.service.TicketService
 import uket.domain.uketevent.service.EntryGroupService
+import uket.domain.uketevent.service.PerformerService
 import uket.domain.uketevent.service.UketEventService
 import uket.domain.user.service.UserService
 import uket.modules.redis.aop.DistributedLock
@@ -16,11 +17,17 @@ class UpdateTicketStatusFacade(
     private val ticketingCompletionMessageSendService: TicketingCompletionMessageSendService,
     private val userService: UserService,
     private val uketEventService: UketEventService,
+    private val performerService: PerformerService,
 ) {
     @DistributedLock(key = "'ticketing' + #entryGroupId")
     fun updateTicketStatus(entryGroupId: Long, ticketId: Long, ticketStatus: TicketStatus, userId: Long): Ticket {
         if (ticketStatus === TicketStatus.RESERVATION_CANCEL) {
             entryGroupService.decreaseReservedCount(entryGroupId)
+
+            val ticket = ticketService.getById(ticketId)
+            val entryGroup = entryGroupService.getById(entryGroupId)
+            val performer = performerService.findByNameAndRoundId(ticket.performerName, entryGroup.uketEventRoundId)
+            performerService.minusTicketCountForPerformer(performer.id, 1);
         }
         if (ticketStatus == TicketStatus.BEFORE_ENTER) {
             val user = userService.getById(userId)
